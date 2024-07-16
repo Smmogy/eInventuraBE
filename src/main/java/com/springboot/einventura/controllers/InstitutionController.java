8e2d54c04716c3e49ff7dfa723eb882b113d628e
Smmogy <zan.zuanovic@gmail.com>
2024-07-16 19:53:18 +0200

    userload

--- src/main/java/com/springboot/einventura/controllers/InstitutionController.java
+++ src/main/java/com/springboot/einventura/controllers/InstitutionController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/institution")
+@RequestMapping("/api/institution")
 @CrossOrigin(origins = "http://localhost:4200")
 @Slf4j
 @Controller
---
+++ src/main/java/com/springboot/einventura/controllers/UserController.java
@@ -0,0 +1,47 @@
+package com.springboot.einventura.controllers;
+
+import com.springboot.einventura.model.bean.User;
+import com.springboot.einventura.model.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userRepository.findAll();
+        return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Integer id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found with id: " + id));
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User createdUser = userRepository.save(user);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Integer id) {
+        userRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
+
